88d6c0c318e7d9c10992ebd8199dbb6e8809c810
kakkar.shivali86@gmail.com <kakkar.shivali86@gmail.com>
2021-09-07 09:41:00 -0400

    Add project files.

---
+++ ConvertTempWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ConvertTempWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConvertTempWebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+        public IActionResult CelsiusToFahrenheit()
+        {
+            return View();
+        }
+        public IActionResult FahrenheitToCelsius()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
